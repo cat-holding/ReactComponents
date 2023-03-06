74ecf2a7a55408edefc87b1e9948f879250a4c5a
Jik789 <stix.jik@gmail.com>
2023-03-06 11:32:57 +0300

    fix: remove huski

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <nav className="nav-top">
       <Link to="/">Главная страница</Link>
-      <Link to="/about">О нас</Link>
+      <Link to="/about">О нас и вас</Link>
       <Link to="/404">Ошибка</Link>
     </nav>
   );
